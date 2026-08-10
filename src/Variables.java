4f370d18fa559d220a9c9bf2f057c881c6c02a48
darshanthosar <darshanthosar99@gmail.com>
2026-08-11 00:13:30 +0530

    Add Java Basics Programs

---
+++ src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    static void main(String[] args) {
+
+        int age=20;
+        double salary=500000.00;
+        String name="Darshan";
+        System.out.println(age);
+        System.out.println(salary);
+        System.out.println(name);
+    }
+}
